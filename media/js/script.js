1a041e0081ef848ad814f56af573657c1aff5f12
Ninian <6039724@mborijnland.nl>
2024-10-23 17:46:15 +0200

    Update script.js

--- media/js/script.js
+++ media/js/script.js
@@ -80,8 +80,8 @@ var cooldown = false;
 var hearts = 3;
 var lastHeartScore = 0;
 
-var maxSpeed = 20;
-var maxSpeed_2 = 25;
+var maxSpeed = 15;
+var maxSpeed_2 = 20;
 
 for (var i = 0; i < carCount; i++) {
     obstacles.push(new Obstacles(i));
